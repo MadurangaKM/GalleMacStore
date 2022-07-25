21a8ed1db988cf1be1c8c0a6381f6f5449086a95
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-07-25 15:52:25 +0530

    footer changes

--- galle-mac-store/components/Footer.jsx
+++ galle-mac-store/components/Footer.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillFacebook,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
+      <div
+        onClick={() => window.open("tel:+94771646687", "_blank")}
+        style={{ cursor: "pointer", color: "#f02d34", fontWeight: 700 }}
+      >
+        <p>Call +94 (77) 1646687</p>
+      </div>
       <p>2022 Galle Mac Store All rights reserverd</p>
       <p className="icons">
         <AiFillInstagram />
+        <AiFillFacebook />
         <AiOutlineTwitter />
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
